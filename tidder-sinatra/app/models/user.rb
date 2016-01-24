c30a8a2065d68178876b3dded9bfa2df8fdfaeb2
TimoSci <me@timosci.net>
2016-01-24 16:46:01 -0500

    plugged RGL into ActiveRecord

--- tidder-sinatra/app/models/user.rb
+++ tidder-sinatra/app/models/user.rb
@@ -1,3 +1,7 @@
+require 'rgl/adjacency'
+require 'rgl/dot'
+require 'rgl/implicit'
+
 class User < ActiveRecord::Base
 
   extend SociableClass
@@ -11,6 +15,35 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :topics
   has_many :comments
+
+  def to_s
+    self.name
+  end
+
+  def self.digraph
+    dg = RGL::DirectedAdjacencyGraph[]
+    self.find_each do |user| # unlike #all, #find_each is an Enumerator that doesn't load the entire set of users into memory
+      dg.add_vertex(user)
+      user.friends.each do |friend|
+        dg.add_edge(user,friend)
+      end
+    end
+    dg
+  end
+
+  def self.implicit_digraph
+    RGL::ImplicitGraph.new { |g|
+       g.vertex_iterator { |b|
+         User.find_each(&b)
+       }
+       g.adjacent_iterator { |x, b|
+         x.friends.each(&b)
+       }
+       g.directed = true
+    }
+  end
+
+
 
   def self.save_dot_file(filename)
     File.open("#{filename}.dot","w") do |f|
@@ -27,9 +60,9 @@ class User < ActiveRecord::Base
         user.friends.each do |friend|
           line = ""
           line << "  "
-          line << user.id.to_s
+          line << user.name.to_s
           line << " -> "
-          line << friend.id.to_s
+          line << friend.name.to_s
           line << ";"
           out << line
           out << "\n"
@@ -39,4 +72,4 @@ class User < ActiveRecord::Base
     out
   end
 
-end
+ end
